ce656e037dd771454024746f42e37738f7345988
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-06-02 09:47:54 +0900

    Add String token

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -125,6 +125,8 @@ func TestNextToken3(t *testing.T) {
 	
 	10 == 10;
 	10 != 9;
+	"foobar"
+	"foo bar"
 	`
 
 	tests := []struct {
@@ -204,6 +206,8 @@ func TestNextToken3(t *testing.T) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
