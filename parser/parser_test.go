f800999221cd79e723ebf7ce019bfec1c4b13d5a
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-06-06 08:52:36 +0900

    parse array literals

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -755,3 +755,28 @@ func TestStringLiteralExpression(t *testing.T) {
 		t.Fatalf("literal.Value is not hello world. got=%s", literal.Value)
 	}
 }
+
+func TestParsingArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	checkParseErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	array, ok := stmt.Expression.(*ast.ArrayLiteral)
+
+	if !ok {
+		t.Fatalf("exp is not *ast.ArrayLiteral. got=%T", stmt.Expression)
+	}
+
+	if len(array.Elements) != 3 {
+		t.Fatalf("len(array.Elements) not 3. got=%d", len(array.Elements))
+	}
+
+	testIntegerLiteral(t, array.Elements[0], 1)
+	testInfixExpression(t, array.Elements[1], 2, "*", 2)
+	testInfixExpression(t, array.Elements[2], 3, "+", 3)
+}
