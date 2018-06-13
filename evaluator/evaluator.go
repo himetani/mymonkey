48c58c0a20ea9699d3ca860cc910812ce7cf2cd0
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-06-13 19:08:33 +0900

    add builtin puts function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -98,6 +98,15 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
+		},
+	},
 }
 
 func isError(obj object.Object) bool {
